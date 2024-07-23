47909be9d8330d07c1f9d3a0f0a180e028876bfa
blancojohn <142819007+blancojohn@users.noreply.github.com>
2024-07-23 17:07:48 +0000

    Practicando condicionales

--- exercises/11-Your_first_if/app.js
+++ exercises/11-Your_first_if/app.js
@@ -1,3 +1,14 @@
 let total = prompt('How many km are left to go?');
 
 // Your code below:
+function showMessaggeToFinish (km){
+    if (km > 100) {
+        return "We still have a bit of driving left to go"
+    } else if (km > 50 && km <= 100){
+        return "We'll be there in 5 minutes"
+    } else if (km <= 50){
+        return "I'm parking. I'll see you right now"
+    }
+}
+
+console.log(showMessaggeToFinish(total))
